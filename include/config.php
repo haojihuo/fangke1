6468c25cf7b67f1221a38e145d7bd0bb11855b69
讯息通网络工作室 <134619102+haojihuo@users.noreply.github.com>
2026-03-02 19:54:21 +0800

    feat: add visitor registration and QR sign-in workflow

---
+++ include/config.php
@@ -0,0 +1,11 @@
+<?php
+$DB_HOST = '127.0.0.1';
+$DB_NAME = 'fangke';
+$DB_USER = 'root';
+$DB_PASS = '';
+
+$base_url = 'http://' . $_SERVER['HTTP_HOST'] . '/';
+
+// 微信公众号配置
+$wechat_appid = 'YOUR_WECHAT_APPID';
+$wechat_secret = 'YOUR_WECHAT_SECRET';
